f164803990ef6056ad24254377374ad489c63024
alternateRath <l.vanhoni@hotmail.com>
2023-10-22 14:18:54 -0300

    v0.0.14
    edit / delete working

--- src/static/script.js
+++ src/static/script.js
@@ -1,89 +1,157 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const postForm = document.getElementById("postForm");
-  const errorMessageContainer = document.getElementById("error-message");
+    const modal = document.getElementById("editModal");
+    const postForm = document.getElementById("postForm");
+    const errorMessageContainer = document.getElementById("error-message");
+    const scheduledPosts = document.getElementById("scheduledPosts");
+    const closeButton = document.querySelector('.close-button');
 
-  const validateFormData = () => {
-    const link = document.getElementById("link").value;
-    const date = document.getElementById("date").value;
-    const time = document.getElementById("time").value;
-
-    if (!link || !date || !time) {
-      return "All fields are required";
+    function getElementSiblingText(element, text) {
+        const matchingElement = Array.from(element.getElementsByTagName('strong')).find(el => el.textContent.includes(text));
+        return matchingElement && matchingElement.nextElementSibling ? matchingElement.nextElementSibling.textContent : null;
     }
 
-    // Add more validation as needed, for example, validate URL format
-    const urlPattern = /^https?:\/\/.+/;
-    if (!urlPattern.test(link)) {
-      return "Please enter a valid URL";
+    function openEditModal(post) {
+        const link = post.querySelector("a").href;
+        const [date, time] = post.querySelector("h3").textContent.split(" ");
+        const legenda = getElementSiblingText(post, 'Legenda');
+        const comentarios = getElementSiblingText(post, 'Comentários');
+        const plataforma = getElementSiblingText(post, 'Plataforma');
+        const postId = post.dataset.id;
+
+        modal.querySelector("#link").value = link;
+        modal.querySelector("#date").value = date;
+        modal.querySelector("#time").value = time;
+        modal.querySelector("#legenda").textContent = legenda;
+        modal.querySelector("#comentarios").textContent = comentarios;
+        modal.querySelector("#plataforma").value = plataforma;
+        modal.querySelector("form").action = `/edit_post/${postId}`;
+        modal.style.display = "block";
     }
 
-    return null;
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const validationError = validateFormData();
-    if (validationError) {
-      errorMessageContainer.textContent = validationError;
-      errorMessageContainer.style.display = "block";
-      return;
+    function closeModal() {
+        modal.style.display = "none";
     }
 
-    // Hide error message container if validation passes
-    errorMessageContainer.style.display = "none";
-
-    const formData = new FormData(postForm);
-    const date = document.getElementById("date").value;
-    const time = document.getElementById("time").value;
-    formData.append("data_hora", `${date} ${time}`);
-
-    try {
-      const response = await fetch("/", { // Adjust the URL if needed
-        method: "POST",
-        body: formData,
-      });
-
-      if (!response.ok) {
-        throw new Error("Server responded with an error");
-      }
-
-      window.location.href = "/agenda";
-    } catch (error) {
-      console.error("Fetch error:", error);
-      errorMessageContainer.textContent = "An error occurred. Please try again.";
-      errorMessageContainer.style.display = "block";
+    function togglePlatformSelection(iconElement) {
+        iconElement.classList.toggle('selected');
+        const selectedPlatforms = Array.from(document.querySelectorAll('.platform-icon.selected')).map(icon => icon.getAttribute('data-platform'));
+        document.getElementById('plataforma').value = selectedPlatforms.join(',');
     }
-  };
 
-  if (postForm) {
-    postForm.addEventListener("submit", handleSubmit);
-  }
+    function handlePostEditClick(event) {
+        const postId = event.currentTarget.dataset.id;
+        const postCard = document.querySelector(`.post-card[data-id="${postId}"]`);
+        openEditModal(postCard);
+    }
 
-  // Initializing the sortable feature for posts
-  const postsList = document.getElementById("postsList");
-  if (postsList) {
-    Sortable.create(postsList, {
-      group: 'posts',
-      animation: 150,
-      onEnd: function (evt) {
-        const order = this.toArray();
-        // Send the new order to the server, adjust the URL and request format as needed
-        fetch('/update_order', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest',
-          },
-          body: JSON.stringify({ new_order: order }),
-        })
-          .then(response => response.json())
-          .then(data => {
-            console.log('Order updated:', data);
-          })
-          .catch((error) => {
-            console.error('Error updating order:', error);
-          });
-      },
+    function handlePostDeleteClick(event) {
+        const postId = event.currentTarget.dataset.id;
+        deletePost(postId);
+    }
+
+    async function deletePost(postId) {
+        try {
+            const response = await fetch(`/delete_post/${postId}`, { method: "DELETE" });
+            if (!response.ok) {
+                throw new Error("Server responded with an error");
+            }
+            document.querySelector(`.post-card[data-id="${postId}"]`).remove();
+            showNotification("Post deleted successfully", "success");
+        } catch (error) {
+            console.error("Error deleting post:", error);
+            showNotification("Error deleting post", "error");
+        }
+    }
+
+    function validateFormData() {
+        const link = document.getElementById("link").value;
+        const date = document.getElementById("date").value;
+        const time = document.getElementById("time").value;
+
+        if (!link || !date || !time) {
+            return "All fields are required";
+        }
+
+        const urlPattern = /^https?:\/\/.+/;
+        if (!urlPattern.test(link)) {
+            return "Please enter a valid URL";
+        }
+
+        return null;
+    }
+
+    async function handleFormSubmit(event) {
+        event.preventDefault();
+        const validationError = validateFormData();
+        if (validationError) {
+            showNotification(validationError, "error");
+            return;
+        }
+
+        const formData = new FormData(postForm);
+        const date = document.getElementById("date").value;
+        const time = document.getElementById("time").value;
+
+        try {
+            const response = await fetch(event.currentTarget.action, { method: "POST", body: formData });
+            if (!response.ok) {
+                throw new Error("Server responded with an error");
+            }
+            window.location.href = "/agenda";
+        } catch (error) {
+            console.error("Fetch error:", error);
+            showNotification("An error occurred. Please try again.", "error");
+        }
+    }
+
+    function showNotification(message, type) {
+        // Implementation depends on how you want to show notifications.
+        // This is just a placeholder function.
+        alert(`${type.toUpperCase()}: ${message}`);
+    }
+
+    if (scheduledPosts) {
+        Sortable.create(scheduledPosts, {
+            group: 'posts',
+            animation: 150,
+            onEnd: function (evt) {
+                const order = this.toArray();
+                fetch('/update_order', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Requested-With': 'XMLHttpRequest',
+                    },
+                    body: JSON.stringify({ new_order: order }),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Order updated:', data);
+                    showNotification("Order updated successfully", "success");
+                })
+                .catch((error) => {
+                    console.error('Error updating order:', error);
+                    showNotification("Error updating order", "error");
+                });
+            },
+        });
+    }
+
+    if (closeButton) {
+        closeButton.addEventListener('click', closeModal);
+    }
+
+    if (postForm) {
+        postForm.addEventListener("submit", handleFormSubmit);
+    }
+
+    document.querySelectorAll(".btn-edit").forEach(button => button.addEventListener("click", handlePostEditClick));
+    document.querySelectorAll(".btn-delete").forEach(button => button.addEventListener("click", handlePostDeleteClick));
+    document.querySelectorAll(".platform-icon").forEach(icon => icon.addEventListener("click", () => togglePlatformSelection(icon)));
+
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            closeModal();
+        }
     });
-  }
 });
