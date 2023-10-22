1b5ca18873c9cb0e7b229359e51168d22d6f551e
alternateRath <l.vanhoni@hotmail.com>
2023-10-22 07:13:42 -0300

    v0.0.12
    no data persistance
    agenda display enhanced

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+<<<<<<< Updated upstream
+=======
+import re
+from datetime import datetime, timedelta
+import json
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
 
@@ -7,8 +13,13 @@ class Post:
         self.link = link
         self.legenda = legenda
         self.horario = horario
+<<<<<<< Updated upstream
         self.comentarios = comentarios or []
         self.plataformas = plataformas or []
+=======
+        self.comentarios = comentarios or ""
+        self.plataforma = plataformas if plataformas is not None else []
+>>>>>>> Stashed changes
 
 agenda = {}
 
@@ -23,10 +34,33 @@ def index():
 
         postagem = Post(link, legenda, horario, comentarios, plataformas)
 
+<<<<<<< Updated upstream
         if horario:
             agenda.setdefault(horario, []).append(postagem)
 
     return render_template("index.html", agenda=agenda)
+=======
+            legenda = request.form.get("legenda")
+            comentarios = request.form.get("comentarios")
+            plataformas = request.form.getlist("plataforma")
+
+            if not link or not re.match(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$", horario):
+                return "Invalid data!", 400
+
+            postagem = Post(link, legenda, horario, comentarios, plataformas)
+            if horario:
+                agenda.setdefault(horario, []).append(postagem)
+
+            return redirect(url_for("agenda_page"))
+
+        return render_template("index.html", today=today, one_week_from_today=one_week_from_today, agenda=agenda)
+    except Exception as e:
+        return str(e), 500
+
+@app.template_filter('datetimeformat')
+def datetimeformat(value, format='%I:%M %p, %B %d, %Y'):
+    return datetime.strptime(value, "%Y-%m-%d %H:%M").strftime(format)
+>>>>>>> Stashed changes
 
 @app.route("/agenda")
 def agenda_page():
--- static/script.js
+++ static/script.js
@@ -1,23 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
     const postForm = document.getElementById("postForm");
 
-    postForm.addEventListener("submit", function(e) {
-        e.preventDefault();
+    const validateFormData = () => {
+        const link = document.getElementById("link").value;
+        const date = document.getElementById("date").value;
+        const time = document.getElementById("time").value;
+
+<<<<<<< Updated upstream:static/script.js
+        const formData = new FormData(postForm);
+=======
+        // Check if link, date, and time values are not empty
+        return link && date && time;
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (!validateFormData()) {
+            alert("Please fill out all fields correctly!");
+            return;
+        }
 
         const formData = new FormData(postForm);
+        const date = document.getElementById("date").value;
+        const time = document.getElementById("time").value;
+        formData.append("data_hora", `${date} ${time}`);
+>>>>>>> Stashed changes:src/static/script.js
 
         fetch("/", {
             method: "POST",
             body: formData,
-        }).then(response => {
-            if (response.ok) {
-                window.location.href = "/agenda";
-            } else {
-                alert("Error scheduling the post. Please try again.");
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Server responded with an error.");
             }
-        }).catch(error => {
+            window.location.href = "/agenda";
+        })
+        .catch(error => {
             console.error("Fetch error:", error);
             alert("An error occurred. Please try again.");
         });
-    });
+    };
+
+    postForm.addEventListener("submit", handleSubmit);
 });
